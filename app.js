ee1ea691327e9f1413ed3d92b5d15ab2acf0a790
Jozsef Komaromi <komjozsef77@gmail.com>
2025-07-10 20:07:25 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const addButton=document.getElementById('add');
+const substractButton=document.getElementById('substract');
+const multiplyButton=document.getElementById('multiply');
+const divideButton=document.getElementById('divide');
+
+const result=document.getElementById('result');
+
+
+addButton.addEventListener('click', ()=>{
+const num1=parseInt(document.getElementById('num1').value)
+const num2=parseInt(document.getElementById('num2').value)
+const operationResult=num1+num2
+
+result.innerHTML=operationResult;
+})
+
+
+substractButton.addEventListener('click', ()=>{
+const num1=parseInt(document.getElementById('num1').value)
+const num2=parseInt(document.getElementById('num2').value)
+const operationResult=num1-num2
+
+result.innerHTML=operationResult;
+})
+
+
+multiplyButton.addEventListener('click', ()=>{
+const num1=parseInt(document.getElementById('num1').value)
+const num2=parseInt(document.getElementById('num2').value)
+const operationResult=num1*num2
+
+result.innerHTML=operationResult;
+})
+
+
+divideButton.addEventListener('click', ()=>{
+const num1=parseInt(document.getElementById('num1').value)
+const num2=parseInt(document.getElementById('num2').value)
+const operationResult=num1/num2
+
+result.innerHTML=operationResult;
+})
+
+
+
+
+
+
