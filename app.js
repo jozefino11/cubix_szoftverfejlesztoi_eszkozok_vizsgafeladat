667a3b4ed65244467de6e84a085f88f88ef8d23e
Jozsef Komaromi <komjozsef77@gmail.com>
2025-07-11 10:37:21 +0200

    Insert a console.log

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
+const addButton = document.getElementById("add");
+const substractButton = document.getElementById("substract");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
 
-const addButton=document.getElementById('add');
-const substractButton=document.getElementById('substract');
-const multiplyButton=document.getElementById('multiply');
-const divideButton=document.getElementById('divide');
+const result = document.getElementById("result");
 
-const result=document.getElementById('result');
+addButton.addEventListener("click", () => {
+  const num1 = parseInt(document.getElementById("num1").value);
+  const num2 = parseInt(document.getElementById("num2").value);
+  const operationResult = num1 + num2;
 
+  result.innerHTML = operationResult;
+});
 
-addButton.addEventListener('click', ()=>{
-const num1=parseInt(document.getElementById('num1').value)
-const num2=parseInt(document.getElementById('num2').value)
-const operationResult=num1+num2
+substractButton.addEventListener("click", () => {
+  const num1 = parseInt(document.getElementById("num1").value);
+  const num2 = parseInt(document.getElementById("num2").value);
+  const operationResult = num1 - num2;
 
-result.innerHTML=operationResult;
-})
+  result.innerHTML = operationResult;
+});
 
+multiplyButton.addEventListener("click", () => {
+  const num1 = parseInt(document.getElementById("num1").value);
+  const num2 = parseInt(document.getElementById("num2").value);
+  const operationResult = num1 * num2;
 
-substractButton.addEventListener('click', ()=>{
-const num1=parseInt(document.getElementById('num1').value)
-const num2=parseInt(document.getElementById('num2').value)
-const operationResult=num1-num2
+  result.innerHTML = operationResult;
+});
 
-result.innerHTML=operationResult;
-})
+divideButton.addEventListener("click", () => {
+  const num1 = parseInt(document.getElementById("num1").value);
+  const num2 = parseInt(document.getElementById("num2").value);
+  const operationResult = num1 / num2;
 
+  console.log("");
 
-multiplyButton.addEventListener('click', ()=>{
-const num1=parseInt(document.getElementById('num1').value)
-const num2=parseInt(document.getElementById('num2').value)
-const operationResult=num1*num2
-
-result.innerHTML=operationResult;
-})
-
-
-divideButton.addEventListener('click', ()=>{
-const num1=parseInt(document.getElementById('num1').value)
-const num2=parseInt(document.getElementById('num2').value)
-const operationResult=num1/num2
-
-result.innerHTML=operationResult;
-})
-
-
-
-
-
-
+  result.innerHTML = operationResult;
+});
